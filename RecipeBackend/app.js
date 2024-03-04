30e618a740eed8afdfc2177d1dfa10b910b93418
No2Cookies <danzemly000@gmail.com>
2024-03-04 11:31:29 +0200

    use

--- RecipeBackend/app.js
+++ RecipeBackend/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = requre('cors');
-
+const cookbookRoute = require('./routes/cookbook.routes');
+const userRoute = require('./routes/user.routes');
+const recipeRoute = require('./routes/recipe.routes');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/v1/cookbook' , cookbookRoute);
+app.use('/api/v1/user' , userRoute);
+app.use('/api/v1/recipe' , recipeRoute);
 
 
 module.exports = { app };
