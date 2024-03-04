7a49f597f8afba93e2ac1e5f78699d230f08acf2
Jonathan Hazan <yonatanhazan1337@gmail.com>
2024-03-04 14:00:32 +0200

    Blalbl

--- RecipeBackend/Models/review.model.js
+++ RecipeBackend/Models/review.model.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 const reviewSchema = mongoose.Schema(
     {
         userId: {type: mongoose.Types.ObjectId, ref: "Users" },
+        recipeId: {type: mongoose.Types.ObjectId, ref: "Recipes" },
         comment:{type:String},
-        rating:[{type:Number}],
+        rating:{type:Number},
 
     }
 )
