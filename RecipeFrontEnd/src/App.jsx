642940646f08be62c97b8b7e17eec96ac62c4032
HilaZagdon <hilatoar@gmail.com>
2024-03-06 14:20:38 +0200

    footer

--- RecipeFrontEnd/src/App.jsx
+++ RecipeFrontEnd/src/App.jsx
@@ -1,21 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react'
-import NavBar from './Components/navBar/NavBar';
+import React, { useContext, useEffect, useState } from "react";
+import NavBar from "./Components/navBar/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { UserContext } from "./context/UserContext";
-import Recipes from './Pages/recipes/Recipes';
-import Dashboard from './Pages/dashboard/Dashboard';
-import SearchPage from './Pages/searchPage/SearchPage';
-import ProfilePage from './Pages/profilePage/ProfilePage';
-import HomePage from './Pages/home/HomePage';
-import Auth from "./Pages/auth/Auth"
-import "./App.css"
-import Footer from './Components/footer/Footer';
-
+import Recipes from "./Pages/recipes/Recipes";
+import Dashboard from "./Pages/dashboard/Dashboard";
+import SearchPage from "./Pages/searchPage/SearchPage";
+import ProfilePage from "./Pages/profilePage/ProfilePage";
+import HomePage from "./Pages/home/HomePage";
+import Auth from "./Pages/auth/Auth";
+import "./App.css";
+import Footer from "./Components/footer/Footer";
 
 function App() {
   const { user, setUser } = useContext(UserContext);
-
-
 
   // const [isLoading, setIsLoading] = useState(true);
   // useEffect(() => {
@@ -55,38 +52,40 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-      <div>
-           <NavBar/>
-      </div>
-      <div>
-      {user ? (
-          <Routes>
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/auth" element={<HomePage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/recipes" element={<Recipes />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            {/* <Route path="*" element={<NotFound />} /> */}
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        ) : (
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/auth" element={<Auth />} />
-            {/* <Route path="*" element={<NotFound />} /> */}
-            <Route path="/dashboard" element={<HomePage />} />
-            <Route path="/recipes" element={<Recipes />} />
-            <Route path="/search" element={<SearchPage/>} />
-            <Route path="/profile" element={<HomePage />} />
-          </Routes>
-        )}
-      </div>
-      <Footer />
-   </BrowserRouter>
+        <div>
+          <NavBar />
+        </div>
+        <div>
+          {user ? (
+            <Routes>
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/auth" element={<HomePage />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/recipes" element={<Recipes />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              {/* <Route path="*" element={<NotFound />} /> */}
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          ) : (
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/auth" element={<Auth />} />
+              {/* <Route path="*" element={<NotFound />} /> */}
+              <Route path="/dashboard" element={<HomePage />} />
+              <Route path="/recipes" element={<Recipes />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/profile" element={<HomePage />} />
+            </Routes>
+          )}
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
