0a6867f987edb39c77f2e74bffd0d5a646a98f12
No2Cookies <danzemly000@gmail.com>
2024-03-07 11:36:56 +0200

    improvements to search component and page

--- RecipeFrontEnd/src/Components/searchPage/searchDisplay.jsx
+++ RecipeFrontEnd/src/Components/searchPage/searchDisplay.jsx
@@ -1,16 +1,23 @@
-
-
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react"
+import './searchDisplay.css'
 
 export default function Sdisplay(props) {
+    const [results, setResults] = useState([]);
 
-
+    useEffect(() => {
+        setResults(props.data);
+    }, [props.data]);
 
     return (
         <section id="searchResultsDisplayWrapper">
+            {results.map((recipe, index) => (
+                <div key={index}>
+                    <Typography variant='h4'>{recipe.name}</Typography>
+                    <Typography variant='subtitle1'>{recipe.desc}</Typography>
 
-
-
-
+                </div>
+            ))}
         </section>
-    )
+    );
 }
