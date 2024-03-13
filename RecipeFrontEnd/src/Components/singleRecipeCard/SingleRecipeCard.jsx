159123252ea181cdaabcb5875b96d3e50f87e701
No2Cookies <danzemly000@gmail.com>
2024-03-13 17:06:09 +0200

    more css for single recipe pag

--- RecipeFrontEnd/src/Components/singleRecipeCard/SingleRecipeCard.jsx
+++ RecipeFrontEnd/src/Components/singleRecipeCard/SingleRecipeCard.jsx
@@ -3,27 +3,39 @@ import './SingleRecipeCard.css'
 import IngredientsCard from './IngredientsCard'
 
 export default function SingleRecipeCard({ recipe }) {
+
+
+
+
+
   return (
-    <div>
+    <div id='singleRecipeCardContainer'>
       <div className="imageRecipeCardDiv">
         <img className="imageRecipeCard" src={"https://b.zmtcdn.com/data/pictures/chains/9/16527959/fe2f25cff9135474e98d851e2e47df05.jpg"} />
       </div>
       <h1 className='nameRecipeCard'>{recipe.name}</h1>
-      <div>
-        <div className='instructions'>
-          <h2>Instructions</h2>
-          {recipe.instructions?.map((instruction) => (
-            <p key={instruction._id}>
-              {instruction.step}: {instruction.instruction}
+
+
+
+
+      <section id='instructionsWrapper'>     <div id='instructions'>
+        <h2>Instructions</h2>
+        {recipe.instructions?.map((instruction) => (
+          <div className='instructionLabel' key={instruction._id}>
+            <span className='instructionNum'>{instruction.step} </span>
+            <p>            {instruction.instruction}
             </p>
-          ))}
-        </div>
-
+          </div>
+        ))}
       </div>
+        <IngredientsCard recipe={recipe}></IngredientsCard>
+      </section>
 
 
 
-      <IngredientsCard recipe={recipe}></IngredientsCard>
+
+
+
     </div>
   )
 }
